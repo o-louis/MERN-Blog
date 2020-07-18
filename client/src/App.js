47b90e288e77549ce7729380df90963177a22736
Oriane <oriane.louis.pro@gmail.com>
2020-07-18 17:53:59 +0200

    Initialize react-app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </Switch>
+        </Router>
+    )
+}
+
+export default App;
